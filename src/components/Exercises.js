2a37e2ab6d6a9cb0a98a5115f1a2cc8f845dca66
Indu-Chandana <indu.99.ic@gmail.com>
2022-09-10 22:28:56 +0530

    Sep10-002

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Button, Box, Stack, TextField, Typography, Pagination } from '@mui/material'
+import ExerciseCard from './ExerciseCard'
 
-const Exercises = () => {
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
   return (
-    <div>Exercises</div>
+    <Box id="exercises"
+      sx={{ mt: { lg: '110px' } }}
+      mt="50px"
+      p="20px">
+      <Typography variant='h3' mb="46px">
+        Showing Results
+      </Typography>
+      <Stack direction="row" sx={{ gap: { lg: '110px', xs: '50px' } }}
+        flexWrap="wrap" justifyContent="center">
+        {exercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination 
+          color='standard'
+          shape='rounded'
+          defaultPage={1}
+          count={Math.ceil(exercises.length / 9)}/>
+        )}
+
+      </Stack>
+    </Box>
   )
 }
 
