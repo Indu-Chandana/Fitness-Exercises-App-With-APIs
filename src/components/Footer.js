71fb5b8f323315d44e32329d953e0b8a8a625de6
Indu-Chandana <indu.99.ic@gmail.com>
2022-09-11 16:34:49 +0530

    Sep11-004

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material';
+
+import Logo from '../assets/images/Logo-1.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="Logo" width="200px" height="40px" />
+        <Typography variant='h5' pb="40px" textAlign="center">
+          Made with ❤️ by Indu Chandana
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
